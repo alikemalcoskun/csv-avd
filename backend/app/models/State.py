bef1c5899b773f93e7879e8dac0c52cff09749b8
alikemalcoskun <alikemalcoskun0@gmail.com>
2025-02-19 22:39:51 +0300

    Add state

---
+++ backend/app/models/State.py
@@ -0,0 +1,22 @@
+from typing import Dict, Any, Optional
+from pydantic import BaseModel, Field
+
+class State(BaseModel):
+    # Generation states
+    query_generate_result: Dict[str, Any] = Field(default_factory=dict)
+    query_execute_result: Dict[str, Any] = Field(default_factory=dict)
+    query_fix_result: Dict[str, Any] = Field(default_factory=dict)
+    
+    # Data agent states
+    data_analyze_result: Dict[str, Any] = Field(default_factory=dict)
+    data_visualize_result: Dict[str, Any] = Field(default_factory=dict)
+    data_tabulate_result: Dict[str, Any] = Field(default_factory=dict)
+
+    # Routing states
+    # https://langchain-ai.github.io/langgraph/tutorials/workflows/#routing
+    data_agent_type_result: str = Field(default="data_analyze")
+    
+    # Evaluator states
+    # https://langchain-ai.github.io/langgraph/tutorials/workflows/#evaluator-optimizer
+    query_eval_result: str = Field(default="pass")
+    query_eval_retries: int = Field(default=0)
