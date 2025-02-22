4769568667480afe7decf2552f05d54ec1b96ace
alikemalcoskun <alikemalcoskun0@gmail.com>
2025-02-22 17:20:34 +0300

    Update DataRouter to connect to LLM

--- backend/app/agents/data_router.py
+++ backend/app/agents/data_router.py
@@ -4,9 +4,12 @@
 
 from pydantic import BaseModel, Field
 from typing_extensions import Literal
+from langchain_core.prompts import ChatPromptTemplate
+
+from app.services.llm_service import LLMService
 
 class Route(BaseModel):
-    step: Literal["analyze", "visualize", "tabulate"] = Field(
+    step: Literal["data_analyze", "data_visualize", "data_tabulate"] = Field(
         None, description="The next step of the data analysis agent"
     )
 
@@ -14,12 +17,34 @@ class Route(BaseModel):
 
 class DataRouter:
     def __init__(self):
-        pass
+        self.llm = LLMService()
 
     def route(self, state: dict) -> dict:
+        messages = [
+            ("system", '''Analyze the user question to determine the type of data analysis to perform.
+            - Data analysis types:
+                - Analyze: Analyzes the data and generates a report.
+                    - Examples: "What is the total sales in the data?", "Gimme a report about the transactions last month."
+                - Visualize: Visualize the data according to the user question.
+                    - Examples: "Plot the sales over time.", "Show me a chart of the sales by product."
+                - Tabulate: Reconstruct the data in a tabular format to show the data on a table.
+                    - Examples: "Show me the data in a table.", "List the sales by product."
+            '''),
+            ("human", '''- User question: {user_question}
+            ''')
+        ]
+        prompt = ChatPromptTemplate(messages)
+
+        response = self.llm.invoke(prompt, is_response_json=False, structured_output=Route,
+            input={
+                "user_question": state.user_question
+            }
+        )
+
         return {
-            "data_agent_type_result": "data_analyze",
+            "data_agent_type_result": response.step
         }
+
 
 
 def data_route_decision(state: dict) -> str:
